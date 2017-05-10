8975c5f0c607fd0efd7d38de790c07af778f33f7
Keren Trajtenberg <kerentrajt@gmail.com>
2017-05-10 11:15:05 +0300

    write default flows

--- odl-arp-learning-impl/src/main/java/org/opendaylight/arplearn/util/FlowUtils.java
+++ odl-arp-learning-impl/src/main/java/org/opendaylight/arplearn/util/FlowUtils.java
@@ -8,8 +8,19 @@
 package org.opendaylight.arplearn.util;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.ActionType;
+import org.opendaylight.genius.mdsalutil.InstructionInfo;
+import org.opendaylight.genius.mdsalutil.InstructionType;
+import org.opendaylight.genius.mdsalutil.MDSALUtil;
+import org.opendaylight.genius.mdsalutil.MatchFieldType;
+import org.opendaylight.genius.mdsalutil.MatchInfo;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
 
 public class FlowUtils {
 
@@ -19,7 +30,38 @@ public class FlowUtils {
 	}
 
 	public static void createDefaultFlows(IMdsalApiManager mdsalApiManager, BigInteger dpnId) {
+		createDropFlow(mdsalApiManager, dpnId);
+		createArpToControllerFlow(mdsalApiManager, dpnId);
 
+	}
+
+	private static void createDropFlow(IMdsalApiManager mdsalApiManager, BigInteger dpnId) {
+		List<MatchInfo> matches = Collections.emptyList();
+		List<InstructionInfo> instructions = new ArrayList<>();
+		List<ActionInfo> actions = new ArrayList<>();
+		actions.add(new ActionInfo(ActionType.drop_action, new String[] {}));
+		instructions.add(new InstructionInfo(InstructionType.apply_actions, actions));
+		installFlow(mdsalApiManager, dpnId, "DefaultDropFlow", "DefaultDropFlow", 1, matches, instructions);
+	}
+
+	private static void createArpToControllerFlow(IMdsalApiManager mdsalApiManager, BigInteger dpnId) {
+		List<MatchInfo> matches = new ArrayList<>();
+		matches.add(new MatchInfo(MatchFieldType.arp_op, new String[] {}));
+		List<InstructionInfo> instructions = new ArrayList<>();
+		List<ActionInfo> actions = new ArrayList<>();
+		actions.add(new ActionInfo(ActionType.punt_to_controller, new String[] {}));
+		instructions.add(new InstructionInfo(InstructionType.apply_actions, actions));
+		installFlow(mdsalApiManager, dpnId, "DefaultArpFlow", "DefaultArpFlow", 1, matches, instructions);
+
+	}
+
+	public static void installFlow(final IMdsalApiManager mdSalManager,
+			final BigInteger dpnId, final String flowId, final String flowName, final int priority,
+			List<MatchInfo> matches, List<InstructionInfo> instructions) {
+
+		final Flow flowEntity = MDSALUtil.buildFlow((short) 0, flowId, priority, flowName, 0, 0, BigInteger.ZERO,
+				matches, instructions);
+		mdSalManager.installFlow(dpnId, flowEntity);
 	}
 
 }
